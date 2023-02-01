b79640225f2d6ec9ba5e76c51be938e82dfe9bfe
Astryda Malinowska <114299751+KyriaFoxy@users.noreply.github.com>
2023-02-01 11:41:53 +0100

    Created git-commit-force.py

---
+++ Python-Scripts/git-commit-force.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import os
+
+os.system("git status")
+
+print("Which file?")
+
+file = input()
+
+os.system(f"git add {file}")
+
+os.system("echo \"What is your commit message?\"")
+
+commit = input()
+
+os.system(f"git commit -m \"{commit}\"")
+
+os.system("git push")
