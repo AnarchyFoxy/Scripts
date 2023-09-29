8c131b89e8544948582119c6eca5e94e1ae6babd
Astryda Malinowska <astamalinowska@outlook.com>
2023-09-29 15:45:04 +0200

    Added snap and flatpak update

--- Python-Scripts/linux-update.py
+++ Python-Scripts/linux-update.py
@@ -8,11 +8,25 @@ def odswiez_liste_repozytoriow():
 def zaktualizuj_system():
     subprocess.run(["sudo", "apt", "upgrade", "-y"])
 
+# Zaktualizuj snap
+def zaktualizuj_snap():
+    subprocess.run(["sudo", "snap", "refresh"])
+
+# Zaktualizuj flatpak
+def zaktualizuj_flatpak():
+    subprocess.run(["flatpak", "update", "-y"])
+
 if __name__ == "__main__":
     print("Rozpoczynam odświeżanie listy repozytoriów...")
     odswiez_liste_repozytoriow()
 
     print("Rozpoczynam aktualizację systemu...")
     zaktualizuj_system()
+
+    print("Rozpoczynam aktualizację snapów...")
+    zaktualizuj_snap()
+
+    print("Rozpoczynam aktualizację flatpaka...")
+    zaktualizuj_flatpak()
 
     print("Aktualizacja zakończona.")
